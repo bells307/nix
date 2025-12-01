5a02f2baf6cd434afbf96765b5012240087b75f4
Bells307 <skyheart307@yandex.ru>
2025-12-01 12:58:48 +0300

    upd

--- darwin.nix
+++ darwin.nix
@@ -47,6 +47,7 @@
     nixfmt
     curl
     taplo
+    lazygit
   ];
 
   # Fonts
--- flake.nix
+++ flake.nix
@@ -3,12 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -20,13 +17,8 @@
         system = "aarch64-darwin";
         specialArgs = { inherit inputs self; };
         modules = [
-          # Main Darwin configuration
           ./darwin.nix
-
-          # Homebrew integration
           nix-homebrew.darwinModules.nix-homebrew
-
-          # Home Manager
           home-manager.darwinModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
